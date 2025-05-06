d2531cc92338439cdc8ed54e86ce82c25b3bd82c
NirmalGaud1 <113794834+NirmalGaud1@users.noreply.github.com>
2025-05-06 12:46:47 +0530

    Update mis_info.py

--- mis_info.py
+++ mis_info.py
@@ -5,12 +5,32 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 
+from keras.layers import Layer
+import tensorflow as tf
+
+class Attention(Layer):
+    def __init__(self, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name='att_weight', shape=(input_shape[-1], 1),
+                                 initializer='normal')
+        self.b = self.add_weight(name='att_bias', shape=(input_shape[1], 1),
+                                 initializer='zeros')        
+        super(Attention, self).build(input_shape)
+
+    def call(self, x):
+        e = tf.keras.backend.tanh(tf.keras.backend.dot(x, self.W) + self.b)
+        a = tf.keras.backend.softmax(e, axis=1)
+        output = x * a
+        return tf.keras.backend.sum(output, axis=1)
+
 # Constants
 VOCAB_SIZE = 10000
 MAX_LENGTH = 100
 
 # Load model
-model = load_model('model_nilesh.h5', compile=False)
+model = load_model('model_nilesh.h5', custom_objects={'Attention': Attention})
 
 # Load tokenizer
 with open('tokenizer.json') as f:
